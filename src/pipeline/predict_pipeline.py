690ef0c489acb25c4ad526926cf70248b86e7f2f
RiteshYennuwar <yennuwarritesh@hotmail.com>
2024-03-18 15:00:16 +0530

    flask application developed

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -26,7 +26,7 @@ class CustomData:
             self,
             gender:str,
             race_ethnicity:str,
-            parental_level_education,
+            parental_level_of_education,
             lunch:str,
             test_preparation_course:str,
             reading_score:int,
@@ -34,7 +34,7 @@ class CustomData:
             ):
         self.gender = gender
         self.race_ethnicity = race_ethnicity
-        self.parental_level_education = parental_level_education
+        self.parental_level_of_education = parental_level_of_education
         self.lunch = lunch
         self.test_preparation_course = test_preparation_course
         self.reading_score = reading_score
@@ -45,7 +45,7 @@ class CustomData:
             custom_data_input_dict = {
                 'gender': [self.gender],
                 'race_ethnicity': [self.race_ethnicity],
-                'parental_level_education': [self.parental_level_education],
+                'parental_level_of_education': [self.parental_level_of_education],
                 'lunch' : [self.lunch],
                 'test_preparation_course' : [self.test_preparation_course],
                 'reading_score' : [self.reading_score],
